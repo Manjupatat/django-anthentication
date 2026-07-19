079df19402bf8f580e44f73e82a4c05e221a8150
manjunathproxelera <manjunath.laxman@proxelera.com>
2026-07-19 12:09:36 +0530

    Integrate PostgreSQL with Django
    - Configure PostgreSQL as the primary database
    - Install psycopg2
    - Update DATABASES configuration
    - Create database and database user
    - Apply Django migrations
    - Verify authentication and connectivity

--- authentication_project/settings.py
+++ authentication_project/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'authentication_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'YourStrongPassword',
+        'HOST': 'localhost',
+        'PORT': '8080',
     }
 }
 
